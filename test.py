713930937c3699b625c13c102ea7640c60ca810b
Rakesh0207Kumar <166150879+Rakesh0207Kumar@users.noreply.github.com>
2024-07-14 10:46:43 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,36 @@
+import boto3
+import io
+from PIL import Image
+
+rekognition = boto3.client('rekognition', region_name='ap-south-1')
+dynamodb = boto3.client('dynamodb', region_name='ap-south-1')
+
+image_path = input("Enter path of the image to check: ")
+
+image = Image.open(image_path)
+stream = io.BytesIO()
+image.save(stream,format="JPEG")
+image.save(stream,format="png")
+image_binary = stream.getvalue()
+
+
+response = rekognition.search_faces_by_image(
+        CollectionId='faceimage_collectionId',    // here you write collection ID , that you created .
+        Image={'Bytes':image_binary}                                       
+        )
+
+found = False
+for match in response['FaceMatches']:
+    print (match['Face']['FaceId'],match['Face']['Confidence'])
+        
+    face = dynamodb.get_item(
+        TableName='image_table',  // you have to write here DynamoDB table name 
+        Key={'RekognitionId': {'S': match['Face']['FaceId']}}
+        )
+    
+    if 'Item' in face:
+        print ("Found Person: ",face['Item']['FullName']['S'])
+        found = True
+
+if not found:
+    print("Person cannot be recognized")
